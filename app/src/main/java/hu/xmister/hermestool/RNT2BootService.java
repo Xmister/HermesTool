260284bc7474792b8b28ae1ddc4a381d02a2db90
Xmister <xmisterhu@gmail.com>
2016-01-11 16:38:00 +0100

    Boot notifications

--- app/src/main/java/hu/xmister/hermestool/RNT2BootService.java
+++ app/src/main/java/hu/xmister/hermestool/RNT2BootService.java
@@ -1,5 +1,8 @@
 package hu.xmister.hermestool;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,11 +10,39 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.List;
+
+import eu.chainfire.libsuperuser.Shell;
+
 public class RNT2BootService extends Service {
     @Override
     public IBinder onBind(Intent arg0) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    private void sendNotify(String text) {
+        // prepare intent which is triggered if the
+// notification is selected
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+// build notification
+// the addAction re-use the same intent to keep the example short
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle("Redmi Note 2 Tool")
+                .setContentText(text)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+                .build();
+
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, n);
     }
 
     @Override
@@ -27,7 +58,17 @@ public class RNT2BootService extends Service {
                 SharedPreferences sharedPreferences =RNT2BootService.this.getSharedPreferences("default", 0);
                 if ( Boolean.valueOf(sharedPreferences.getString("cbAutoMount","false"))) {
                     Log.i("Boot Service-MT", "Mounting SD card...");
-                    SUCommand.mountSD();
+                    SUCommand.mountSD(new Shell.OnCommandResultListener() {
+                        @Override
+                        public void onCommandResult(int commandCode, int exitCode, List<String> output) {
+                            if ( exitCode > 0 ) {
+                                sendNotify(getString(R.string.mount_error));
+                            }
+                            else {
+                                sendNotify(getString(R.string.mount_success));
+                            }
+                        }
+                    });
                     Log.i("Boot Service-MT", "Done");
                 }
                 try {
@@ -37,9 +78,20 @@ public class RNT2BootService extends Service {
                     Log.i("Boot Service-IT", "Updating values...");
                     if (Constants.getFrequencyNames() == null)  {
                         Log.e("Boot Service-IT", "Couldn't load frequency values...");
+                        sendNotify(getString(R.string.freq_error));
                     }
                     else {
-                        SUCommand.interTweak(RNT2BootService.this, null);
+                        SUCommand.interTweak(RNT2BootService.this, new Shell.OnCommandResultListener() {
+                            @Override
+                            public void onCommandResult(int commandCode, int exitCode, List<String> output) {
+                                if ( exitCode > 0 ) {
+                                    sendNotify(getString(R.string.apply_error));
+                                }
+                                else {
+                                    sendNotify(getString(R.string.apply_success));
+                                }
+                            }
+                        });
                     }
                     Log.i("Boot Service-IT", "Done");
                 }
