c76698d35d58669d417a6697cc2afb4b055ebc33
Xmister <xmisterhu@gmail.com>
2016-01-18 19:49:37 +0100

    Add MIUI default

--- app/src/main/java/hu/xmister/hermestool/MainActivity.java
+++ app/src/main/java/hu/xmister/hermestool/MainActivity.java
@@ -8,6 +8,8 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
